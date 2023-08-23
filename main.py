e41f6f626c6329221ea8a65bdb969ae082271f84
Sharan Kumar <shrnk9@gmail.com>
2023-08-23 12:25:59 +0530

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlproject import logger
 from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -21,7 +22,7 @@ try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataValidationTrainingPipeline()
         obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx====================================x")
 except Exception as e:
         logger.exception(e)
         raise e
@@ -33,7 +34,19 @@ try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataTransformationTrainingPipeline()
         obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx====================================x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx====================================x")
 except Exception as e:
         logger.exception(e)
         raise e
